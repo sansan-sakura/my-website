5e2e669a014355f145c9b384856fd84d6fde49a7
Sakura Tanaka <sito6496t@google.com>
2023-09-19 09:11:25 +0900

    some config for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
-
+import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   "[astro]": {
-    "editor.defaultFormatter": "esbenp.prettier-vscode"
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
   },
   integrations: [react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  adapter: netlify(),
 });
